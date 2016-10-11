adcec7cb13a08b1b39b2de0b8bf0bc4c0227d322
SAITOU Keita <keita44.f4@gmail.com>
2016-10-11 20:40:13 +0900

    Add: example of tableView by TableModel

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,11 +1,13 @@
 #include "mainwindow.hpp"
 #include "ui_mainwindow.h"
 #include "qtablemodel.hpp"
+#include "tablemodel.hpp"
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
   ui(new Ui::MainWindow),
-  qtmodel(new QTableModel)
+  qtmodel(new QTableModel(this)),
+  tmodel(new TableModel(0, 2, this))
 {
   ui->setupUi(this);
 
@@ -18,6 +20,18 @@ MainWindow::MainWindow(QWidget *parent) :
     qtmodel->setData(index1, tr("My Address"));
   }
   ui->tableView->setModel(qtmodel);
+
+  // TableModel
+  tmodel->setHeaderData(0, Qt::Horizontal, tr("Name"), Qt::DisplayRole);
+  tmodel->setHeaderData(1, Qt::Horizontal, tr("Address"), Qt::DisplayRole);
+  for (int i = 0; i < 3; ++i) {
+    tmodel->insertRows(i, 1);
+    QModelIndex index0 = tmodel->index(i, 0);
+    QModelIndex index1 = tmodel->index(i, 1);
+    tmodel->setData(index0, tr("My Name"));
+    tmodel->setData(index1, tr("My Address"));
+  }
+  ui->tableView_2->setModel(tmodel);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -3,6 +3,7 @@
 
 #include <QMainWindow>
 #include "qtablemodel.hpp"
+#include "tablemodel.hpp"
 
 namespace Ui {
 class MainWindow;
@@ -19,6 +20,7 @@ public:
 private:
   Ui::MainWindow *ui;
   QTableModel *qtmodel;
+  TableModel *tmodel;
 };
 
 #endif // MAINWINDOW_HPP
