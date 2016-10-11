59d24c1306f437e6a7bf044ff12ee440bc3d7a18
SAITOU Keita <keita44.f4@gmail.com>
2016-10-11 19:39:57 +0900

    Refactor: fix indent

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,13 +3,13 @@
 #include "qtablemodel.hpp"
 
 MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent),
-    ui(new Ui::MainWindow)
+  QMainWindow(parent),
+  ui(new Ui::MainWindow)
 {
-    ui->setupUi(this);
+  ui->setupUi(this);
 }
 
 MainWindow::~MainWindow()
 {
-    delete ui;
+  delete ui;
 }
