9a3da66da0005512d1f268d534d4f955f4e21663
SAITOU Keita <keita44.f4@gmail.com>
2016-10-11 20:00:37 +0900

    Add: new tree view by QTableModel

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -4,9 +4,20 @@
 
 MainWindow::MainWindow(QWidget *parent) :
   QMainWindow(parent),
-  ui(new Ui::MainWindow)
+  ui(new Ui::MainWindow),
+  qtmodel(new QTableModel)
 {
   ui->setupUi(this);
+
+  // QTableModel
+  for (int i = 0; i < 3; ++i) {
+    qtmodel->insertRows(i, 1);
+    QModelIndex index0 = qtmodel->index(i, 0);
+    QModelIndex index1 = qtmodel->index(i, 1);
+    qtmodel->setData(index0, tr("My Name"));
+    qtmodel->setData(index1, tr("My Address"));
+  }
+  ui->tableView->setModel(qtmodel);
 }
 
 MainWindow::~MainWindow()
--- mainwindow.hpp
+++ mainwindow.hpp
@@ -2,6 +2,7 @@
 #define MAINWINDOW_HPP
 
 #include <QMainWindow>
+#include "qtablemodel.hpp"
 
 namespace Ui {
 class MainWindow;
@@ -17,6 +18,7 @@ public:
 
 private:
   Ui::MainWindow *ui;
+  QTableModel *qtmodel;
 };
 
 #endif // MAINWINDOW_HPP
